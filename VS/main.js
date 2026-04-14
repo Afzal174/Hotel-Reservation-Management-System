d9a68efa4840c597c8cccca8f2c78a2a241e6f7d
Mohamed Afzal <mohamedafzal525@gmail.com>
2026-04-14 20:03:51 +0530

    Added Google Drive video player

--- VS/main.js
+++ VS/main.js
@@ -144,3 +144,7 @@ window.onpopstate = function(event) {
         loadPage(event.state.path);
     }
 };
+function changeVideo(id) {
+    document.querySelector(".video-container iframe").src =
+    "https://drive.google.com/file/d/" + id + "/preview";
+}
